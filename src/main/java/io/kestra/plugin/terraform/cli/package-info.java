71de72334637403fa5a8afb766c0ea508fe51a58
ben8t <pimpaudben@gmail.com>
2025-12-30 15:42:56 +0100

    chore: update plugin categories

--- src/main/java/io/kestra/plugin/terraform/cli/package-info.java
+++ src/main/java/io/kestra/plugin/terraform/cli/package-info.java
@@ -1,6 +1,9 @@
 @PluginSubGroup(
     description = "This sub-group of plugins contains tasks for using Terraform.\n",
-    categories = PluginSubGroup.PluginCategory.TOOL
+    categories = PluginSubGroup.PluginCategory.TOOL,
+    categories = {
+        PluginSubGroup.PluginCategory.INFRASTRUCTURE
+    }
 )
 package io.kestra.plugin.terraform;
 
